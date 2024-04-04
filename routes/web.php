b2855af7365d54b9a8c65aa51c2e5901b452656b
ahmedessouaied <ahmed.essouaied@supcom.tn>
2024-04-04 01:26:16 +0100

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/catalogue', function () {
+    return view('catalogue');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/singlearticle', function () {
+    return view('singlearticle');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/profile', function () {
+    return view('profile');
 });
